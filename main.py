61f2f4ffaab40fe5bd37aa5bc609bcfe1269b381
Rakshanda26 <rakshandamm123@gmail.com>
2023-11-09 14:20:09 +0530

    data_transformation_completed

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from sensor.entity import config_entity
 from sensor.entity.config_entity import DataIngestionConfig,TrainingPipelineConfig
 from sensor.components.data_ingestion import DataIngestion
 from sensor.components.data_validation import DataValidation
+from sensor.components.data_transformation import DataTransformation
 
 
 
@@ -26,17 +27,29 @@ if __name__=="__main__":
         #test_logger_and_exception()
         get_collection_as_dataframe(database_name= "aps", collection_name="sensor")
         training_pipeline_config= config_entity.TrainingPipelineConfig()
+
+        #data ingestion
         data_ingestion_config = DataIngestionConfig(training_pipeline_config=training_pipeline_config)
         #print(data_ingestion_config.to_dict())
         data_ingestion=DataIngestion(data_ingestion_config=data_ingestion_config)
         print(data_ingestion.initiate_data_ingestion())
         data_ingestion_artifact = data_ingestion.initiate_data_ingestion()
         
-        #
+        #data validation
         data_validation_config=config_entity.DataValidationConfig(training_pipeline_config=training_pipeline_config)
         data_validation= DataValidation(data_validation_config=data_validation_config, 
                        data_ingestion_artifact=data_ingestion_artifact)
         data_validation_artifact=data_validation.initiate_data_validation()
+        print(data_validation_artifact)
+
+
+        #data transformation
+        data_transformation_config = config_entity.DataTransformationConfig(training_pipeline_config=training_pipeline_config)
+        data_transformation = DataTransformation(data_transformation_config=data_transformation_config, 
+        data_ingestion_artifact=data_ingestion_artifact)
+        data_transformation_artifact = data_transformation.initiate_data_transformation()
+        print(data_transformation_artifact)
+
 
 
 
